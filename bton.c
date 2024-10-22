78628ed29e3cd815269ab8b71c00b13b2278d135
JaCier8 <jappa414@gmail.com>
2024-10-22 22:48:42 +0200

    Drugie kody

---
+++ bton.c
@@ -0,0 +1,16 @@
+// //
+// // Created by jappa on 20/10/2024.
+// //
+// #include <stdio.h>
+// #include <stdlib.h>
+// int main(){
+//     int N; scanf("%d", &N);
+//     int A[N]; for(int i = 0; i < N; i++) scanf("%d", &A[i]);
+//     int wynik;
+//     // Mamy ustalone N, zawartość tablicy A oraz zadeklarowaną zmienną wynik
+//     // ---------------------------------------------------------------------
+//     // Poniżej wpisz swój fragment programu:
+//     // ---------------------------------------------------------------------
+//     printf("%d\n", wynik);
+//     return 0;
+// }
